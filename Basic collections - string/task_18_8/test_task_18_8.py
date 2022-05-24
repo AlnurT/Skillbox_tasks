a1741422ada37e6207cb3412b51bd609eb49927c
Alnur <alnur.tazhbenov@mail.ru>
2022-05-24 14:11:54 +0300

    Task 18.8: Initial commit

---
+++ Basic collections - string/task_18_8/test_task_18_8.py
@@ -0,0 +1,10 @@
+import pytest
+from task_18_8.task_18_8 import check_shift_of_string
+
+
+@pytest.mark.parametrize(
+    ("some_string", "shift"),
+    [("abcde", 0), ("bcdea", 1), ("eabcd", 1), ("cdeab", 2), ("edcba", -1)],
+)
+def test_check_shift_of_string(some_string, shift):
+    assert check_shift_of_string("abcde", some_string) == shift
