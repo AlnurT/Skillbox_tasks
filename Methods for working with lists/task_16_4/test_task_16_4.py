3a38cb202db6eabae892d6f7781c3971e8e7414d
Alnur <alnur.tazhbenov@mail.ru>
2022-05-30 15:51:50 +0300

    Task 16.4: Using function for changing list of names and simplification of function of action

--- Methods for working with lists/task_16_4/test_task_16_4.py
+++ Methods for working with lists/task_16_4/test_task_16_4.py
@@ -1,16 +1,29 @@
 import pytest
-from task_16_4.task_16_4 import is_action_in_guests_list
+from task_16_4.task_16_4 import adding_or_removing_guest, is_action_in_guests_list
 
 
 @pytest.mark.parametrize(
     ("guests_num", "action", "is_action"),
     [
-        (0, "пришёл", True),
-        (6, "пришёл", False),
-        (6, "ушёл", True),
-        (0, "ушёл", False),
-        (0, "пора спать", False),
+        (0, "in", True),
+        (6, "in", False),
+        (6, "out", True),
+        (0, "out", False),
+        (0, "time to sleep", False),
     ],
 )
-def test_add_or_remove_guests(guests_num, action, is_action):
+def test_is_action_in_guests_list(guests_num, action, is_action):
     assert is_action_in_guests_list(guests_num, action) == is_action
+
+
+@pytest.mark.parametrize(
+    ("guests_list", "guest_name", "in_or_out", "new_guests_list"),
+    [
+        ([], "Talgat", "in", ["Talgat"]),
+        (["Talgat"], "Talgat", "out", []),
+    ],
+)
+def test_adding_or_removing_guest(guests_list, guest_name, in_or_out, new_guests_list):
+    assert (
+        adding_or_removing_guest(guests_list, guest_name, in_or_out) == new_guests_list
+    )
